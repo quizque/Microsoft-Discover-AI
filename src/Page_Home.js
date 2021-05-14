6695cc17e8db9fac648f530da4340964408e0dea
Quizque <13785390+quizque@users.noreply.github.com>
2021-05-14 01:24:08 -0400

    Moddified home page

--- src/Page_Home.js
+++ src/Page_Home.js
@@ -1,7 +1,25 @@
 const Home = () => {
     return (
-        <div>
-            <h1 className="center-temp">Coming soon! (Home Page)</h1>
+        <div className="home-divider">
+            <div className="home-left">
+                <h1>
+                    The Worlds First{" "}
+                    <span className="home-highlight">AI Powered</span> Therapist
+                </h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse vel rutrum massa, sit amet dictum mi. Fusce eros
+                    mi, vulputate eu tortor vitae, condimentum bibendum nunc.
+                    Etiam erat ex, dictum tempus augue dignissim, porttitor
+                    consequat diam.
+                </p>
+            </div>
+            <div>
+                <img
+                    src={require("/assets/app_placeholder.png")}
+                    alt="iphone placeholder"
+                ></img>
+            </div>
         </div>
     );
 };
--- src/Page_MeetTheTeam.js
+++ src/Page_MeetTheTeam.js
@@ -1,62 +1,71 @@
 const MeetTheTeam = () => {
     return (
-        <div>
+        <div className="meet-the-team">
+            <h1>Meet The Team</h1>
             <div className="id-card-row">
-                <div className="id-card">
-                    <img
-                        src={require("/assets/profile1.png")}
-                        alt="Profile"
-                    ></img>
-                    <h1>William Banks</h1>
-                    <h3>Fullstack Developer</h3>
-                    <p>
-                        orem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Nullam turpis elit, imperdiet id libero feugiat,
-                        hendrerit consectetur ligula. Maecenas vel ex ante. Duis
-                        sed diam vitae arcu porttitor porta eu ut purus.
-                    </p>
+                <div className="id-card-unfold">
+                    <div className="id-card">
+                        <img
+                            src={require("/assets/profile1.png")}
+                            alt="Profile"
+                        ></img>
+                        <h1>William Banks</h1>
+                        <h3>Fullstack Developer</h3>
+                        <p>
+                            orem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Nullam turpis elit, imperdiet id libero
+                            feugiat, hendrerit consectetur ligula. Maecenas vel
+                            ex ante. Duis sed diam vitae arcu porttitor porta eu
+                            ut purus.
+                        </p>
+                    </div>
+                    <div className="id-card">
+                        <img
+                            src={require("/assets/profile2.png")}
+                            alt="Profile"
+                        ></img>
+                        <h1>Isabelle Leach</h1>
+                        <h3>Lead Designer</h3>
+                        <p>
+                            orem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Nullam turpis elit, imperdiet id libero
+                            feugiat, hendrerit consectetur ligula. Maecenas vel
+                            ex ante. Duis sed diam vitae arcu porttitor porta eu
+                            ut purus.
+                        </p>
+                    </div>
                 </div>
-                <div className="id-card">
-                    <img
-                        src={require("/assets/profile2.png")}
-                        alt="Profile"
-                    ></img>
-                    <h1>Isabelle Leach</h1>
-                    <h3>Lead Designer</h3>
-                    <p>
-                        orem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Nullam turpis elit, imperdiet id libero feugiat,
-                        hendrerit consectetur ligula. Maecenas vel ex ante. Duis
-                        sed diam vitae arcu porttitor porta eu ut purus.
-                    </p>
-                </div>
-                <div className="id-card">
-                    <img
-                        src={require("/assets/profile3.png")}
-                        alt="Profile"
-                    ></img>
-                    <h1>Tegan Chan</h1>
-                    <h3>AI Designer</h3>
-                    <p>
-                        orem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Nullam turpis elit, imperdiet id libero feugiat,
-                        hendrerit consectetur ligula. Maecenas vel ex ante. Duis
-                        sed diam vitae arcu porttitor porta eu ut purus.
-                    </p>
-                </div>
-                <div className="id-card">
-                    <img
-                        src={require("/assets/profile4.png")}
-                        alt="Profile"
-                    ></img>
-                    <h1>Isaac Harris</h1>
-                    <h3>AI Designer</h3>
-                    <p>
-                        orem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Nullam turpis elit, imperdiet id libero feugiat,
-                        hendrerit consectetur ligula. Maecenas vel ex ante. Duis
-                        sed diam vitae arcu porttitor porta eu ut purus.
-                    </p>
+                <div className="id-card-unfold">
+                    <div className="id-card">
+                        <img
+                            src={require("/assets/profile3.png")}
+                            alt="Profile"
+                        ></img>
+                        <h1>Tegan Chan</h1>
+                        <h3>AI Designer</h3>
+                        <p>
+                            orem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Nullam turpis elit, imperdiet id libero
+                            feugiat, hendrerit consectetur ligula. Maecenas vel
+                            ex ante. Duis sed diam vitae arcu porttitor porta eu
+                            ut purus.
+                        </p>
+                    </div>
+                    <div className="id-card">
+                        <img
+                            src={require("/assets/profile4.png")}
+                            alt="Profile"
+                        ></img>
+                        <h1>Isaac Harris</h1>
+                        <h3>AI Designer</h3>
+                        <p>
+                            orem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Nullam turpis elit, imperdiet id libero
+                            feugiat, hendrerit consectetur ligula. Maecenas vel
+                            ex ante. Duis sed diam vitae arcu porttitor porta eu
+                            ut purus.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
